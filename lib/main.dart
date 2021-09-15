02a3b1ba082b20193368a74bf634520f80405953
AdnaneEz-zaim <ezzaimadnane1361@gmail.com>
2021-09-15 16:14:58 +0100

    commit project BMI

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
         primarySwatch: Colors.pink,
+
       ),
       home: const HomeScreen(),
     );
